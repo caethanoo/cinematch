0701dda919f41b63382193b1cfd4d637fac8f9c9
Brennda Caethano <bcaethano21@gmail.com>
2025-07-23 23:07:44 -0300

    feat: Implement security module for password hashing and JWTs
    Adds the core/security.py module to handle core authentication logic.
    
    Implements password hashing and verification using Passlib with the bcrypt algorithm.
    
    Adds a function to create JWT access tokens, signed with the HS256 algorithm.
    
    Refactors the create_user CRUD function to use the new get_password_hash helper.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,35 +1,19 @@
 from pydantic_settings import BaseSettings
-import os
-from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
-from jose import JWTError, jwt
-from core.config import settings 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Settings(BaseSettings):
    
-    PROJECT_NAME: str
-    PROJECT_VERSION: str
+    PROJECT_NAME: str = "CineMatch"
+    PROJECT_VERSION: str = "0.1.0"
+    
+    
     SECRET_KEY: str
+    
+    
+    DATABASE_URL: str 
 
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8' 
 
-
 settings = Settings()
-
-
-SECRET_KEY = settings.SECRET_KEY
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30 # O token valerá por 30 minutos
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
 
